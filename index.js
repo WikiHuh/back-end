16e216d0553d119f6116231f55d90c872a02e961
Rebecca Swernofsky <rswernofsky@gmail.com>
2020-02-08 14:15:05 -0500

    add socket basics

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express')();
+// var router = express.Router();
+var http = require('http').createServer(express);
+var io = require('socket.io')(http);
+
+express.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
